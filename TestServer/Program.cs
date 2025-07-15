998273ea718f810fd7247584844343ac13071cb1
antonfirsov <antonfir@gmail.com>
2025-07-16 01:44:47 +0200

    initial mess

---
+++ TestServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int Port = 8080;
+IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, Port);
+
+using Socket socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+socket1.Bind(endpoint);
+socket1.Listen();
+using Socket client = socket1.Accept();
+
