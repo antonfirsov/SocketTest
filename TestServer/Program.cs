bb5c8fdb5cf9975e39eb3262c8437d33c7d7a038
antonfirsov <antonfir@gmail.com>
2025-07-16 17:41:10 +0200

    compose clients

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
-const int Port = 8080;
-IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, Port);
+IPAddress address = IPAddress.Parse(Environment.GetEnvironmentVariable("CONTAINER_IP"));
+const int Port = 5000;
+IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
 
-using Socket socket1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+if (!host.AddressList.Contains(address))
+{
+    Console.WriteLine($"Container IP {address} not found in host address list.");
+    return -1;    
+}
+
+IPEndPoint endpoint = new IPEndPoint(address, Port);
+using Socket socket1 = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 socket1.Bind(endpoint);
 socket1.Listen();
-using Socket client = socket1.Accept();
 
+Console.WriteLine($"Listening on {endpoint}...");
+
+while (true)
+{
+    Socket s = socket1.Accept();
+    Console.WriteLine($"Accepted connection from {s.RemoteEndPoint}");
+}
