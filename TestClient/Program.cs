7492666fec130e4e5ec125412f8b58672d7e51e6
antonfirsov <antonfir@gmail.com>
2025-07-16 17:53:59 +0200

    client skeleton

---
+++ TestClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+await Task.Delay(1000); // Allow time for server to start
+
+const int Port = 5000;
+string[] addressStrings = ["172.20.0.10", "172.20.0.11", "2001:db8:1::10", "2001:db8:1::11"];
+foreach (IPAddress a in addressStrings.Select(IPAddress.Parse))
+{
+    Socket socket = new Socket(a.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    IPEndPoint endpoint = new IPEndPoint(a, Port);
+    System.Console.WriteLine($"Connecting to {endpoint}...");
+    Stopwatch sw = Stopwatch.StartNew();
+    await socket.ConnectAsync(endpoint);
+    System.Console.WriteLine($"Connected to {endpoint} in {sw.ElapsedMilliseconds} ms");
+}
